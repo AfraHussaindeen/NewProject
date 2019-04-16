f211cadfca93dd95ee8dd0e234e6868fb657c94c
Afra <mhfafra@gmail.com>
2019-04-16 12:12:38 +0530

    designing home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,4 +1,131 @@
 
+<?php $this->start('head');?>
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+<link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+<link rel="stylesheet" href="css/animate.css">
+
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<link rel="stylesheet" href="css/aos.css">
+
+<link rel="stylesheet" href="css/ionicons.min.css">
+
+<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+<link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+<link rel="stylesheet" href="css/flaticon.css">
+<link rel="stylesheet" href="css/icomoon.css">
+<link rel="stylesheet" href="css/style.css">
+<?php $this->end();?>
+
+
 <?php $this->start('body'); ?>
-<h1 class="text-center red">Welcome Afra</h1>
+
+<section class="home-slider owl-carousel">
+      <div class="slider-item" style="background-image:url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-md-6 text ftco-animate">
+            <h1 class="mb-4">Helping Your <span>Stay Happy One</span></h1>
+            <h3 class="subheading">Everyday We Bring Hope and Smile to the Patient We Serve</h3>
+            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">View our works</a></p>
+          </div>
+        </div>
+        </div>
+      </div>
+
+      <div class="slider-item" style="background-image:url(images/bg_2.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-md-6 text ftco-animate">
+            <h1 class="mb-4">We Care <span>About Your Health</span></h1>
+            <h3 class="subheading">Your Health is Our Top Priority with Comprehensive, Affordable medical.</h3>
+            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">View our works</a></p>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-services ftco-no-pb">
+			<div class="container">
+				<div class="row no-gutters">
+          <div class="col-md-3 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block text-center">
+              <div class="icon d-flex justify-content-center align-items-center">
+            		<span class="flaticon-doctor"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Qualitfied Doctors</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block text-center">
+              <div class="icon d-flex justify-content-center align-items-center">
+            		<span class="flaticon-ambulance"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Emergency Care</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>    
+          </div>
+          <div class="col-md-3 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block text-center">
+              <div class="icon d-flex justify-content-center align-items-center">
+            		<span class="flaticon-stethoscope"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Outdoor Checkup</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block text-center">
+              <div class="icon d-flex justify-content-center align-items-center">
+            		<span class="flaticon-24-hours"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">24 Hours Service</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+        </div>
+			</div>
+		</section>
+		
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+<!-- <script src="js/popper.min.js"></script> -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- <script src="js/jquery.easing.1.3.js"></script> -->
+  <script src="js/jquery.waypoints.min.js"></script>
+  <!-- <script src="js/jquery.stellar.min.js"></script> -->
+  <!-- <script src="js/owl.carousel.min.js"></script> -->
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> -->
+  <!-- <script src="js/google-map.js"></script> -->
+  <script src="js/main.js"></script>
+
+
 <?php $this->end(); ?>
