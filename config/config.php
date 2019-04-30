e58829cfde349415f6f175d22a6c264b0a6cc2f7
Afra <mhfafra@gmail.com>
2019-04-30 09:39:05 +0530

    Started including state design pattern

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ define ("DEFAULT_CONTROLLER","Home"); //default value if there isn't one define
 define ('DEBUG',true);
 define('DEFAULT_LAYOUT','default');// if no layout is set in the controller use this $_layout
 
-define('SITE_TITLE','Afra mvc framework');//This will be used if no site title is SITE_TITLE
+define('SITE_TITLE','TCService');//This will be used if no site title is SITE_TITLE
 define ('PROOT','/NewProject/');// set this to '/' for a live server'
 
 define('CURRENT_USER_SESSION_NAME','kwXCMFYjdsndmcjNGDmXhDCnhHc');//session name for a logged in user
