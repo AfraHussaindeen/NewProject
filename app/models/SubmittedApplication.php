fd1d54edff88e03fa1b4fe89aadfc0589a00e28d
AfraHussaindeen <47135592+AfraHussaindeen@users.noreply.github.com>
2019-05-10 10:10:50 +0530

    Update SubmittedApplication.php

--- app/models/SubmittedApplication.php
+++ app/models/SubmittedApplication.php
@@ -7,12 +7,6 @@ class Training extends Model{
         parent::__construct($table);
         $this->_softDelete=true;
     }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 09d4dcc509cab799111b72258a3e04b77778f998
-
-    
    public function findAllByUserId($id,$params=[]){
     $conditions=[
         "conditions"=>'id= ?',
@@ -20,20 +14,7 @@ class Training extends Model{
     ];
     $conditions=array_merge($conditions,$params);
     return $this->find($conditions);
-}
-<<<<<<< HEAD
-}
-=======
-}
-=======
-    public function updateNotifCount($result_query){
-        $count = mysqli_num_rows($result_query);
-        return $count;
     }
 }
     
-    
-    
-    
->>>>>>> 1296dbc26c6e5239b96d8192b722d591184c8663
->>>>>>> 09d4dcc509cab799111b72258a3e04b77778f998
+   
