d0f83c04e8eb0a845ba4ddc429ea436021a65904
NuwanthikaWaihenage <47145340+NuwanthikaW96@users.noreply.github.com>
2019-05-23 11:43:44 +0530

    include the relevent data for the document details

--- app/views/documentDetails/index.php
+++ app/views/documentDetails/index.php
@@ -4,3 +4,196 @@
 <?php $this->start('body');?>
   <h1 class='text-center red '> Details of the Documents that need to be submit...............</h1>
 <?php $this->end();?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Document Details</title>
+    <link rel="stylesheet" href="personal_details.css" type="text/css">
+   
+</head>
+
+<body>
+    
+ <h1 style="color:darkred;">DOCUMENT DETAILS</h1>
+    
+    <a href="#Promotion to Grade I">Promotion to Grade I</a>
+    <br>
+    <a href="#Promotion to Grade II">Promotion to Grade II</a>
+    <br>
+    <a href="#Confirmation in Post">Confirmation in Post</a>
+    <br>
+    <a href="#Salary conversion">Salary conversion</a>
+    <br>
+    <a href="#Salary Increment">Salary Increment</a>
+    <br>
+    <a href="#Car permit Applicatoin">Car permit Applicatoin</a>
+    <br>
+    <a href="#Foreign leave Application form">Foreign leave Application form</a>
+    <br>
+    <a href="#Language allowance">Language allowance</a>
+    <br>
+    <a href="#Maternity Leave">Maternity Leave</a>
+    <br>
+    <a href="#Permanent Release">Permanent Release</a>
+    <br>
+    <a href="#Resignation">Resignation</a>
+    <br>
+    <a href="#Research Allowance">Research Allowance</a>
+    <br>
+    <a href="#Change name">Change name</a>
+    <br>
+    <a href="#Amendment of date of foreign leave">Amendment of date of foreign leave</a>
+    <br>
+    <a href="#Service letter">Service letter</a>
+    <br>
+    <a href="#Service record">Service record</a>
+
+    <br>
+    <a name="Promotion to Grade I"><h2>Promotion to Grade I</h2></a>
+    <ul>
+        <li>Application form</li>
+        <li>Letter from PGIM</li>
+        <li>Grade II letter</li>
+        <li>Certified copy of National ID</li>
+        <li>Fullfillment of 6 years of active service</li>
+    </ul>
+
+    <a name="Promotion to Grade II"><h2>Promotion to Grade II</h2></a>
+    <ul>
+        <li>Application form</li>
+        <li>Certified copy of O/L</li>
+        <li>E bar results sheet</li>
+        <li>Gen 160</li>
+        <li>Gen 278</li>
+        <li>Gen 261</li>
+        <li>Gen 169</li>
+        <li>H 169</li>
+        <li>Certified copy of NIC</li>
+        <li>Oath</li>
+    </ul>
+
+    <a name="Confirmation in Post"><h2>Confirmation in Post</h2></a>
+    <ul>
+        <li>Certified copy of O/L</li>
+        <li>Gen 160</li>
+        <li>Gen 278</li>
+        <li>Gen 261</li>
+        <li>Gen 169</li>
+        <li>H 169</li>
+        <li>Certified copy of NIC</li>
+        <li>Oath</li>
+        <li>Appendix 05 forms for 3 years</li>
+    </ul>
+
+    <a name="Salary conversion"><h2>Salary conversion</h2></a>
+    <ul>
+        <li>Increment form  for every passing year</li>
+    </ul>
+
+    <a name="Salary Increment"><h2>Salary Increment</h2></a>
+    <ul>
+        <li>Increment forms </li>
+        <li>G 185</li>
+    </ul>
+
+    <a name="Car permit Applicatoin"><h2>Car permit Applicatoin</h2></a>
+    <ul>
+        <li>3 copies of Application form</li>
+        <li>Original  of Previous LC</li>
+    </ul>
+
+    <a name="Foreign leave Application form"><h2>Foreign leave Application form</h2></a>
+    <ul>
+        <li>2 copies of Gen 126</li>
+        <li>Letter from PGIM</li>
+        <li>Leave details</li>
+        <li>Cover up signed</li>
+    </ul>
+
+    <a name="Language allowance"><h2>Language allowance</h2></a>
+    <ul>
+        <li>Application form</li>
+        <li>Certified copy of A/L</li>
+        <li>School leaving certificate</li>
+        <li>Certified copy of degree certificate</li>
+    </ul>
+
+    <a name="Maternity Leave"><h2>Maternity Leave</h2></a>
+    <ul>
+        <li>Full pay</li>
+        <ul>
+            <li>Request letter</li>
+            <li>Original medical certificate</li>
+        </ul>
+
+        <li>Half Pay</li>
+        <ul>
+            <li>Request letter</li>
+            <li>Copy of birth certificate</li>
+        </ul>
+        <li>No pay</li>
+        <ul>
+            <li>Request letter</li>
+        </ul>
+    </ul>
+
+
+    <a name="Permanent Release"><h2>Permanent Release</h2></a>
+    <ul>
+        <li>Request letter </li>
+        <li>Appointment letter</li>
+        <li>Appendix  10</li>
+        <li>Affidavit</li>
+        <li>Approval letter prepared by SC</li>
+    </ul>
+
+    <a name="Resignation"><h2>Resignation</h2></a>
+    <ul>
+        <li>Request letter</li>
+        <li>Approval letter prepared by SC</li>
+        <li>Letter from finance branch</li>
+    </ul>
+
+    <a name="Research Allowance"><h2>Research Allowance</h2></a>
+    <ul>
+        <li>Application form</li>
+    </ul>
+
+    <a name="Change name"><h2>Change name</h2></a>
+    <ul>
+        <li>Application form</li>
+        <li>Copy of birth certificate of applicant</li>
+        <li>Copy of birth certificate of spouse</li>
+        <li>Copy of NIC of applicant</li>
+        <li>Copy of NIC of spouse</li>
+    </ul>
+
+    <a name="Amendment of date of foreign leave"><h2>Amendment of date of foreign leave</h2></a>
+    <ul>
+        <li>Request letter</li>
+    </ul>
+
+    <a name="Service letter"><h2>Service letter</h2></a>
+    <ul>
+        <li>Request letter</li>
+    </ul>
+
+    <a name="Service record"><h2>Service record</h2></a>
+    <ul>
+        <li>Request letter</li>
+        <li>Appointment letters</li>
+    </ul>
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
