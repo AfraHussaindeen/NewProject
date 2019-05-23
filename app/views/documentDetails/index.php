8923632e0f5b7dd66c120764e376b683b17aa492
Afra <mhfafra@gmail.com>
2019-05-23 21:09:05 +0530

    created the document details interface

--- app/views/documentDetails/index.php
+++ app/views/documentDetails/index.php
@@ -2,5 +2,202 @@
 <?php $this->setSiteTitle('Document Details');?>
 
 <?php $this->start('body');?>
-  <h1 class='text-center red '> Details of the Documents that need to be submit...............</h1>
+
+<div class="container">
+  
+  <div class =" container mx-auto well " > 
+    <h1 style="color:#0c366dfb;text-align:center;font-family:Georgia, sans-serif ; font-weight:bold ; font-size: 23px  ;height: 50px">DOCUMENT DETAILS</h1>
+    <h4 style="font-family:Georgia, sans-serif ; font-size: 18px;text-align:center">Details of the Documents that need to be handover when applying for ,</h4>
+    <ul  style="padding-left:30% ;font-family:Georgia, sans-serif ; font-size: 17px" >
+        <li ><a href="#Promotion to Grade I">Promotion to Grade I</a></li>
+        <li ><a href="#Promotion to Grade II">Promotion to Grade II</a></li>
+        <li ><a href="#Confirmation in Post">Confirmation in Post</a></li>
+        <li ><a href="#Salary conversion">Salary conversion</a></li>
+        <li ><a href="#Salary Increment">Salary Increment</a></li>
+        <li ><a href="#Car permit Applicatoin">Car permit Applicatoin</a></li>
+        <li ><a href="#Foreign leave Application form">Foreign leave Application form</a></li>
+        <li ><a href="#Language allowance">Language allowance</a></li>
+        <li ><a href="#Maternity Leave">Maternity Leave</a></li>
+        <li ><a href="#Permanent Release">Permanent Release</a></li>
+        <li ><a href="#Resignation">Resignation</a></li>
+        <li ><a href="#Research Allowance">Research Allowance</a></li>
+        <li ><a href="#Change name">Change name</a></li>
+        <li ><a href="#Amendment of date of foreign leave">Amendment of date of foreign leave</a></li>
+        <li ><a href="#Service letter">Service letter</a></li>
+        <li ><a href="#Service record">Service record</a></li>
+    </ul>
+  </div>
+ 
+  <div class =" container mx-auto well "style=";font-family:Georgia, sans-serif "  >
+        
+    <a name="Promotion to Grade I" class="font_type" >Promotion to Grade I</a>
+    <ul  style="font_type-list" >
+        <li>Application form</li>
+        <li>Letter from PGIM</li>
+        <li>Grade II letter</li>
+        <li>Certified copy of National ID</li>
+        <li>Fullfillment of 6 years of active service</li>
+    </ul>
+    <br>
+    <a name="Promotion to Grade II"  class="font_type">Promotion to Grade II</a>
+    <ul style="font_type-list">
+        <li>Application form</li>
+        <li>Certified copy of O/L</li>
+        <li>E bar results sheet</li>
+        <li>Gen 160</li>
+        <li>Gen 278</li>
+        <li>Gen 261</li>
+        <li>Gen 169</li>
+        <li>H 169</li>
+        <li>Certified copy of NIC</li>
+        <li>Oath</li>
+    </ul>
+    <br>
+    <a name="Confirmation in Post" class="font_type" >Confirmation in Post</a>
+    <ul style="font_type-list">
+        <li>Certified copy of O/L</li>
+        <li>Gen 160</li>
+        <li>Gen 278</li>
+        <li>Gen 261</li>
+        <li>Gen 169</li>
+        <li>H 169</li>
+        <li>Certified copy of NIC</li>
+        <li>Oath</li>
+        <li>Appendix 05 forms for 3 years</li>
+    </ul>
+    <br>
+
+    <a name="Salary conversion" class="font_type">Salary conversion</a>
+    <ul style="font_type-list">
+        <li>Increment form  for every passing year</li>
+    </ul>
+    <br>
+
+    <a name="Salary Increment" class="font_type">Salary Increment</a>
+    <ul style="font_type-list">
+        <li>Increment forms </li>
+        <li>G 185</li>
+    </ul>
+    <br>
+
+    <a name="Car permit Applicatoin" class="font_type">Car permit Applicatoin</a>
+    <ul style="font_type-list">
+        <li>3 copies of Application form</li>
+        <li>Original  of Previous LC</li>
+    </ul>
+    <br>
+
+    <a name="Foreign leave Application form" class="font_type">Foreign leave Application form</a>
+    <ul style="font_type-list">
+        <li>2 copies of Gen 126</li>
+        <li>Letter from PGIM</li>
+        <li>Leave details</li>
+        <li>Cover up signed</li>
+    </ul>
+    <br>
+
+    <a name="Language allowance" class="font_type">Language allowance</a>
+    <ul style="font_type-list">
+        <li>Application form</li>
+        <li>Certified copy of A/L</li>
+        <li>School leaving certificate</li>
+        <li>Certified copy of degree certificate</li>
+    </ul>
+    <br>
+
+    <a name="Maternity Leave" class="font_type">Maternity Leave</a>
+    <ul style="font_type-list">
+        <li>Full pay</li>
+        <ul>
+            <li>Request letter</li>
+            <li>Original medical certificate</li>
+        </ul>
+
+        <li>Half Pay</li>
+        <ul>
+            <li>Request letter</li>
+            <li>Copy of birth certificate</li>
+        </ul>
+        <li>No pay</li>
+        <ul>
+            <li>Request letter</li>
+        </ul>
+    </ul>
+    <br>
+
+    <a name="Permanent Release" class="font_type">Permanent Release</a>
+    <ul style="font_type-list">
+        <li>Request letter </li>
+        <li>Appointment letter</li>
+        <li>Appendix  10</li>
+        <li>Affidavit</li>
+        <li>Approval letter prepared by SC</li>
+    </ul>
+    <br>
+
+    <a name="Resignation" class="font_type">Resignation</a>
+    <ul style="font_type-list">
+        <li>Request letter</li>
+        <li>Approval letter prepared by SC</li>
+        <li>Letter from finance branch</li>
+    </ul>
+    <br>
+
+    <a name="Research Allowance" class="font_type">Research Allowance</a>
+    <ul style="font_type-list">
+        <li>Application form</li>
+    </ul>
+    <br>
+
+    <a name="Change name" class="font_type">Change name</a>
+    <ul style="font_type-list">
+        <li>Application form</li>
+        <li>Copy of birth certificate of applicant</li>
+        <li>Copy of birth certificate of spouse</li>
+        <li>Copy of NIC of applicant</li>
+        <li>Copy of NIC of spouse</li>
+    </ul>
+    <br>
+
+    <a name="Amendment of date of foreign leave" class="font_type">Amendment of date of foreign leave</a>
+    <ul style="font_type-list">
+        <li>Request letter</li>
+    </ul>
+    <br>
+
+    <a name="Service letter" class="font_type">Service letter</a>
+    <ul style="font_type-list">
+        <li>Request letter</li>
+    </ul>
+    <br>
+
+    <a name="Service record" class="font_type">Service record</a>
+    <ul style="font_type-list">
+        <li>Request letter</li>
+        <li>Appointment letters</li>
+    </ul>
+
+
+
+  </div>
+
+
+
+
+</div>
+<style>
+    
+.font_type{
+    font-family:Georgia,sans-serif ;
+      font-size: 17px
+}
+.font_type-list{
+    font-family:Georgia,
+     sans-serif ;
+      font-size:18px
+}
+</style>
+
+
 <?php $this->end();?>
+
