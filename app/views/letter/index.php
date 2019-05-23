a43c4839dc400155c58fa7bb2d412f927c230e49
Hasini Weerasooriya <41505060+HasiniRangana@users.noreply.github.com>
2019-05-23 23:18:43 +0530

    created interface for the tables

--- app/views/letter/index.php
+++ app/views/letter/index.php
@@ -1,38 +1,40 @@
 
 <?php $this->setSiteTitle('Submitted Letters');?>
+<html lang="en">
 
 <?php $this->start('body'); ?>
-
+<link rel="stylesheet" type="text/css" href="css/main.css">
 <h1 class="text-center ">Submitted Letters </h1>
-<table class="table table-striped table-condensed table-bordered">
+<div class="container">
+  <table class="responsive-table">
     <thead>
     
-        <th>Name with Initials</th>
-        <th>NIC Number</th>
-        <th>Reg Number</th>
-        <th>Date of Stage1</th>
-        <th>Date of Stage2</th>
-        <th>Date of Stage3</th>
-        <th>Date of Stage4</th>
-        <th>Date of Stage5</th>
-        <th></th>
+        <th scope="col">Name with Initials</th>
+        <th scope="col">NIC Number</th>
+        <th scope="col">Reg Number</th>
+        <th scope="col">Date of Stage1</th>
+        <th scope="col">Date of Stage2</th>
+        <th scope="col">Date of Stage3</th>
+        <th scope="col">Date of Stage4</th>
+        <th scope="col">Date of Stage5</th>
+        <th scope="col"></th>
         
     </thead>
 
     <tbody>
         <?php foreach($this->submissions as $submission): ?>
             <tr>
-            <td><?= $submission->nameInitial ?></td>
-            <td><?= $submission->nicNumber ?></td>
-            <td><?= $submission->regNumber ?></td>
-            <td><?= $submission->dateStage1 ?></td>
-            <td><?= $submission->dateStage2 ?></td>
-            <td><?= $submission->dateStage3 ?></td>
-            <td><?= $submission->dateStage4 ?></td>
-            <td><?= $submission->dateStage5 ?></td>
-            <td>
+            <td data-title= "Name with Initials"><?= $submission->nameInitial ?></td>
+            <td data-title="NIC Number"><?= $submission->nicNumber ?></td>
+            <td data-title="Reg Number"><?= $submission->regNumber ?></td>
+            <td data-title=Date of Stage1><?= $submission->dateStage1 ?></td>
+            <td data-title=Date of Stage2><?= $submission->dateStage2 ?></td>
+            <td data-title=Date of Stage3><?= $submission->dateStage3 ?></td>
+            <td data-title=Date of Stage4><?= $submission->dateStage4 ?></td>
+            <td data-title=Date of Stage5><?= $submission->dateStage5 ?></td>
+            <td data-title=>
                 <a href="<?=PROOT?>letter/delete/<?=$submission->id?>" class="btn  btn-danger btn-xs" onclick="if(!confirm('Are you sure that you want to delete?')){return false;}">
-                    <i class="glyphicon glyphicon-remove "></i>Delete
+                    <i class="glyphicon glyphicon-trash"></i>Delete
                 </a>
                 <a href="<?=PROOT?>letter/nextStage/<?=$submission->id?>" class="btn  btn-info btn-xs" onclick="if(!confirm('Are you sure that you want to move to the next stage?')){return false;}">
                     </i>Next
@@ -41,7 +43,8 @@
             </tr>
         <?php endforeach; ?>
     </tbody>
-
+    </table>
+</div>
 
 <?php $this->end(); ?>
 
