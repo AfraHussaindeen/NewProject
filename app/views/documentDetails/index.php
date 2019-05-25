c48d168d6d265e4e84087019b60676216cad3d44
NuwanthikaWaihenage <47145340+NuwanthikaW96@users.noreply.github.com>
2019-05-25 10:55:35 +0530

    edit document details and add css

--- app/views/documentDetails/index.php
+++ app/views/documentDetails/index.php
@@ -28,17 +28,19 @@
     </ul>
   </div>
    
-    <a name="Promotion to Grade I"><h2>Promotion to Grade I</h2></a>
-    <ul>
+     <div class =" container mx-auto well "style=";font-family:Georgia, sans-serif "  >
+        
+    <a name="Promotion to Grade I" class="font_type" >Promotion to Grade I</a>
+    <ul  style="font_type-list" >
         <li>Application form</li>
         <li>Letter from PGIM</li>
         <li>Grade II letter</li>
         <li>Certified copy of National ID</li>
         <li>Fullfillment of 6 years of active service</li>
     </ul>
-
-    <a name="Promotion to Grade II"><h2>Promotion to Grade II</h2></a>
-    <ul>
+    <br>
+    <a name="Promotion to Grade II"  class="font_type">Promotion to Grade II</a>
+    <ul style="font_type-list">
         <li>Application form</li>
         <li>Certified copy of O/L</li>
         <li>E bar results sheet</li>
@@ -50,9 +52,9 @@
         <li>Certified copy of NIC</li>
         <li>Oath</li>
     </ul>
-
-    <a name="Confirmation in Post"><h2>Confirmation in Post</h2></a>
-    <ul>
+    <br>
+    <a name="Confirmation in Post" class="font_type" >Confirmation in Post</a>
+    <ul style="font_type-list">
         <li>Certified copy of O/L</li>
         <li>Gen 160</li>
         <li>Gen 278</li>
@@ -63,42 +65,48 @@
         <li>Oath</li>
         <li>Appendix 05 forms for 3 years</li>
     </ul>
+    <br>
 
-    <a name="Salary conversion"><h2>Salary conversion</h2></a>
-    <ul>
+    <a name="Salary conversion" class="font_type">Salary conversion</a>
+    <ul style="font_type-list">
         <li>Increment form  for every passing year</li>
     </ul>
+    <br>
 
-    <a name="Salary Increment"><h2>Salary Increment</h2></a>
-    <ul>
+    <a name="Salary Increment" class="font_type">Salary Increment</a>
+    <ul style="font_type-list">
         <li>Increment forms </li>
         <li>G 185</li>
     </ul>
+    <br>
 
-    <a name="Car permit Applicatoin"><h2>Car permit Applicatoin</h2></a>
-    <ul>
+    <a name="Car permit Applicatoin" class="font_type">Car permit Applicatoin</a>
+    <ul style="font_type-list">
         <li>3 copies of Application form</li>
         <li>Original  of Previous LC</li>
     </ul>
+    <br>
 
-    <a name="Foreign leave Application form"><h2>Foreign leave Application form</h2></a>
-    <ul>
+    <a name="Foreign leave Application form" class="font_type">Foreign leave Application form</a>
+    <ul style="font_type-list">
         <li>2 copies of Gen 126</li>
         <li>Letter from PGIM</li>
         <li>Leave details</li>
         <li>Cover up signed</li>
     </ul>
+    <br>
 
-    <a name="Language allowance"><h2>Language allowance</h2></a>
-    <ul>
+    <a name="Language allowance" class="font_type">Language allowance</a>
+    <ul style="font_type-list">
         <li>Application form</li>
         <li>Certified copy of A/L</li>
         <li>School leaving certificate</li>
         <li>Certified copy of degree certificate</li>
     </ul>
+    <br>
 
-    <a name="Maternity Leave"><h2>Maternity Leave</h2></a>
-    <ul>
+    <a name="Maternity Leave" class="font_type">Maternity Leave</a>
+    <ul style="font_type-list">
         <li>Full pay</li>
         <ul>
             <li>Request letter</li>
@@ -115,53 +123,69 @@
             <li>Request letter</li>
         </ul>
     </ul>
+    <br>
 
-
-    <a name="Permanent Release"><h2>Permanent Release</h2></a>
-    <ul>
+    <a name="Permanent Release" class="font_type">Permanent Release</a>
+    <ul style="font_type-list">
         <li>Request letter </li>
         <li>Appointment letter</li>
         <li>Appendix  10</li>
         <li>Affidavit</li>
         <li>Approval letter prepared by SC</li>
     </ul>
+    <br>
 
-    <a name="Resignation"><h2>Resignation</h2></a>
-    <ul>
+    <a name="Resignation" class="font_type">Resignation</a>
+    <ul style="font_type-list">
         <li>Request letter</li>
         <li>Approval letter prepared by SC</li>
         <li>Letter from finance branch</li>
     </ul>
+    <br>
 
-    <a name="Research Allowance"><h2>Research Allowance</h2></a>
-    <ul>
+    <a name="Research Allowance" class="font_type">Research Allowance</a>
+    <ul style="font_type-list">
         <li>Application form</li>
     </ul>
+    <br>
 
-    <a name="Change name"><h2>Change name</h2></a>
-    <ul>
+    <a name="Change name" class="font_type">Change name</a>
+    <ul style="font_type-list">
         <li>Application form</li>
         <li>Copy of birth certificate of applicant</li>
         <li>Copy of birth certificate of spouse</li>
         <li>Copy of NIC of applicant</li>
         <li>Copy of NIC of spouse</li>
     </ul>
+    <br>
 
-    <a name="Amendment of date of foreign leave"><h2>Amendment of date of foreign leave</h2></a>
-    <ul>
+    <a name="Amendment of date of foreign leave" class="font_type">Amendment of date of foreign leave</a>
+    <ul style="font_type-list">
         <li>Request letter</li>
     </ul>
+    <br>
 
-    <a name="Service letter"><h2>Service letter</h2></a>
-    <ul>
+    <a name="Service letter" class="font_type">Service letter</a>
+    <ul style="font_type-list">
         <li>Request letter</li>
     </ul>
+    <br>
 
-    <a name="Service record"><h2>Service record</h2></a>
-    <ul>
+    <a name="Service record" class="font_type">Service record</a>
+    <ul style="font_type-list">
         <li>Request letter</li>
         <li>Appointment letters</li>
     </ul>
+
+
+
+  </div>
+
+
+
+
+</div>
+<style>
 
 <?php $this->end();?>
 
