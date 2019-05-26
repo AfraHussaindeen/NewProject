4660d1e62d43baf818898afe381d2f9fa0367c42
Yogya <yogyagamage@gmail.com>
2019-05-26 18:50:44 +0530

    changed the rescricted page

--- app/views/restricted/index.php
+++ app/views/restricted/index.php
@@ -1,5 +1,5 @@
 <?php $this->setSiteTitle(' Access restricted ');?>
 
 <?php $this->start('body');?>
-  <h1 class='text-center red '> You do not have permission to cccess this page.</h1>
+  <h1 class='text-center red '> You do not have permission to access this page.</h1>
 <?php $this->end();?>
