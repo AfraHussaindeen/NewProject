d9866e4321328d25b221c38c51cabaa161b937bc
NuwanthikaWaihenage <47145340+NuwanthikaW96@users.noreply.github.com>
2019-05-30 10:38:46 +0530

    fixed the error in displaying footer

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,7 +1,7 @@
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<link rel="stylesheet" href="<?PROOT?>css/main.css">
+<link rel="stylesheet" href="NewProject/css/main.css">
 
 <footer class="site-footer">
       
