012420a216c8cb97c159bc80e3738ead5cf65a4f
NuwanthikaWaihenage <47145340+NuwanthikaW96@users.noreply.github.com>
2019-07-14 21:33:50 +0530

    complete footer php

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,26 +1,70 @@
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="<?php echo PROOT ?>/css/main.css">
 
-  <footer class="site-footer">
+<style>
+.site1-footer {
+  position:static;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: black;
+  color:	#87CEFA;
+  text-align: center;
+  padding: 15px;
+  
+}
+.fa {
+  padding: 10px;
+  font-size: 20px;
+  width: 40px;
+  text-align: center;
+  text-decoration: none;
+  margin: 2px 2px;
+  border-radius: 60%;
+}
+
+.fa:hover {
+    opacity: 0.5;
+    
+}
+
+}
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-dribble {
+  background:pink;
+  color: white;
+}
+
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+}
+</style>
+
+<footer class="site1-footer">
       
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
+         
             <p class="copyright-text">Copyright &copy; 2019 All Rights Reserved by 
-         <a href="#">Ministry of Health</a>.
-            </p>
-          </div>
-
-          <div class="col-md-4 col-sm-6 col-xs-12">
+         <a href="#">Ministry of Health</a>
+            
             <ul class="social-icons">
-              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+              <a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+              <a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+              <a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a>
+              <a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a>
             </ul>
-          </div>
-        </div>
-      </div>
+            </p>
 </footer>
+     
      
